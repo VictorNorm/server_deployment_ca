2e834f52f08e046af054c378448256ff6dc7541c
VictorNorm <VictorNorm@users.noreply.github.com>
2023-05-08 12:48:49 +0200

    added email validation

--- routes/participants.js
+++ routes/participants.js
@@ -2,11 +2,25 @@ var express = require("express");
 var router = express.Router();
 const jsend = require("jsend");
 router.use(jsend.middleware);
+const CyclicDB = require("@cyclic.sh/dynamodb");
+const db = CyclicDB(process.env.CYCLIC_DB);
+let participants = db.collection("participants");
 
 /* GET home page. */
-router.post("/", function (req, res, next) {
-  const test = req.body.email;
-  res.jsend.success(test);
+router.post("/", async function (req, res, next) {
+  const { email, firstName, lastName, dob, active } = req.body;
+  const isValidEmail = emailRegex.test(email);
+
+  if (!email || isValidEmail == false) {
+    return res.jsend.fail("You must provide a valid email");
+  }
+  await users.set(email, {
+    firstName: firstName,
+    secondName: lastName,
+    dob: dob,
+    active: active,
+  });
+  res.jsend.success("You successfully added a record");
 });
 
 module.exports = router;
