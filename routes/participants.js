c3202cf5634fa254fe428669c366e7c52b89cbc3
VictorNorm <VictorNorm@users.noreply.github.com>
2023-05-08 12:15:13 +0200

    testing post route

--- routes/participants.js
+++ routes/participants.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const jsend = require("jsend");
+router.use(jsend.middleware);
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.post("/", function (req, res, next) {
+  const test = req.body.email;
+  res.jsend.success(test);
 });
 
 module.exports = router;
